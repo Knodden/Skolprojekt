3f29994c0915a9be984e7dfcc207613d26b5d561
André Persson <knodden@live.se>
2018-10-24 12:52:11 +0200

    Påbörjat timer grejen.

--- RSSreader/RSSreader/BusinessLayer/Interval.cs
+++ RSSreader/RSSreader/BusinessLayer/Interval.cs
@@ -3,11 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Timers;
 
 namespace RSSreader.BusinessLayer
 {
     class Interval
     {
+		private static System.Timers.Timer intervalTimer;
 		// Tar in en String och en Int för att skapa en timer som uppdaterar
+		//public static void StartTimer(string url, int interval) {
+		//	int intervalSeconds = interval * 60;
+		//	intervalTimer = new System.Timers.Timer(intervalSeconds);
+		//}
     }
 }
