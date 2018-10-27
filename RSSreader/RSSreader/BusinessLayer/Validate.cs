f12cc3b36635a462898fcedfd90b346e92f3b808
Björn Jonsson <bjorn_jonsson2@hotmail.com>
2018-10-27 18:03:50 +0200

    Url validering

--- RSSreader/RSSreader/BusinessLayer/Validate.cs
+++ RSSreader/RSSreader/BusinessLayer/Validate.cs
@@ -10,6 +10,29 @@ namespace RSSreader.BusinessLayer
     {
 		public void IsURL(string URL) {
 			// Kontrollera att string URL är en länk.
+        {
+			if (URL.Contains(@"^(http|https|ftp|)\://|[a-zA-Z0-9\-\.]+\.[a-zA-Z](:[a-zA-Z0-9]*)?/?([a-zA-Z0-9\-\._\?\,\'/\\\+&amp;%\$#\=~])*[^\.\,\)\(\s]$"))
+            {
+                return true;
+            }
+           else
+           {
+               return false;
+           }
+                
+        bool NotEmpty(string Input) 
+        {
+            if(Input == !null) 
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
 		}
     }
+ }
+}
+
 }
