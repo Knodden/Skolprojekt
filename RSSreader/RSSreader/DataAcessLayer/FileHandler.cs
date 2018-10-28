8b67470c167771064ff8f7fe416a57ad8794a7cf
Björn Jonsson <bjorn_jonsson2@hotmail.com>
2018-10-28 16:31:24 +0100

    FileHandler

--- RSSreader/RSSreader/DataAcessLayer/FileHandler.cs
+++ RSSreader/RSSreader/DataAcessLayer/FileHandler.cs
@@ -3,11 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using System.Xml;
 
 namespace RSSreader.DataAcessLayer
 {
     class FileHandler
     {
+        
+        
+        public static bool Exists ()
+        {
+            var dir = @"C:\Users\Dator\source\repos\Skolprojekt\RSSreader\RSSreader\DataAcessLayer\XMLFiles\";
+            var filePath1 = dir + "\\Category.xml";
+            var filePath2 = dir + "\\Podcast.xml";
+            if(!Dictionary.Exists(dir)) 
+            {
+                Dictionary.CreateDirectory(dir);
+            }
+            if(!File.Exists(filePath1 || filePath2)) 
+            {
+                File.Create(filePath1 || filePath2);
+            }
+        }
         
     }
 }
