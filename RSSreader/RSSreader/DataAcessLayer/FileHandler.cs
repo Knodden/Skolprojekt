648af199e5bcc05a07476d74f441323e3f1f18b8
åsa <aasa.haakansson@gmail.com>
2018-10-29 16:16:39 +0100

    Lagt till klassen Episode. Gjort ändringar i FileHandler.

--- RSSreader/RSSreader/DataAcessLayer/FileHandler.cs
+++ RSSreader/RSSreader/DataAcessLayer/FileHandler.cs
@@ -12,20 +12,25 @@ namespace RSSreader.DataAcessLayer
     {
         
         
-        //public static bool Exists ()
-        //{
-        //    var dir = @"C:\Users\Dator\source\repos\Skolprojekt\RSSreader\RSSreader\DataAcessLayer\XMLFiles\";
-        //    var filePath1 = dir + "\\Category.xml";
-        //    var filePath2 = dir + "\\Podcast.xml";
-        //    if(!Dictionary.Exists(dir)) 
-        //    {
-        //        Dictionary.CreateDirectory(dir);
-        //    }
-        //    if(!File.Exists(filePath1 || filePath2)) 
-        //    {
-        //        File.Create(filePath1 || filePath2);
-        //    }
-        //}
-        
+        public void Exists ()
+        {
+            var dir = @"C:\Users\Dator\source\repos\Skolprojekt\RSSreader\RSSreader\DataAcessLayer\XMLFiles\";
+            var filePath1 = dir + "\\Category.xml";
+            var filePath2 = dir + "\\Podcast.xml";
+            if(!Directory.Exists(dir)) 
+           {
+                Directory.CreateDirectory(dir);
+           }
+               if(!File.Exists(filePath1)) 
+               {
+                    File.Create(filePath1);
+               }
+
+               if (!File.Exists(filePath2))
+               {
+                File.Create(filePath2);
+               }
+        }
+
     }
 }
